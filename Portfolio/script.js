37e8ccd0200ed9b8810de3fe30e137a84c2e568f
yurisokolovicz <yurisokolovicz@gmail.com>
2023-02-24 10:35:14 -0300

    critical update

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -10,6 +10,11 @@ document.querySelector('.navbar-nav').addEventListener('click', function (e) {
     }
 });
 
+const gitHubButtonWeb = document.getElementById('gitHub-button-web');
+gitHubButtonWeb.addEventListener('click', function () {
+    window.location.href = 'https://github.com/yurisokolovicz';
+});
+
 const tindogButtonWeb = document.getElementById('tindog-button-web');
 tindogButtonWeb.addEventListener('click', function () {
     window.location.href = 'https://yurisokolovicz.github.io//TinDog/';
@@ -22,10 +27,40 @@ tindogButtonDoc.addEventListener('click', function () {
 
 const natoursButtonWeb = document.getElementById('natours-button-web');
 natoursButtonWeb.addEventListener('click', function () {
-    window.location.href = 'https://github.com/yurisokolovicz/Natours/tree/main/Natours/';
+    window.location.href = 'https://yurisokolovicz.github.io//Natours/';
 });
 
 const natoursButtonDoc = document.getElementById('natours-button-doc');
 natoursButtonDoc.addEventListener('click', function () {
     window.location.href = 'https://github.com/yurisokolovicz/Natours';
+});
+
+const guessMyNumberButtonWeb = document.getElementById('guess_my_number-button-web');
+guessMyNumberButtonWeb.addEventListener('click', function () {
+    window.location.href = 'https://yurisokolovicz.github.io//GuessMyNumber/';
+});
+
+const guessMyNumberButtonDoc = document.getElementById('guess_my_number-button-doc');
+guessMyNumberButtonDoc.addEventListener('click', function () {
+    window.location.href = 'https://github.com/yurisokolovicz/Guess-My-Number';
+});
+
+const piggametButtonWeb = document.getElementById('pig_game-button-web');
+piggametButtonWeb.addEventListener('click', function () {
+    window.location.href = 'https://yurisokolovicz.github.io//PigGame/';
+});
+
+const piggameButtonDoc = document.getElementById('pig_game-button-doc');
+piggameButtonDoc.addEventListener('click', function () {
+    window.location.href = 'https://github.com/yurisokolovicz/Pig-Game';
+});
+
+const bankistButtonWeb = document.getElementById('bankist-button-web');
+bankistButtonWeb.addEventListener('click', function () {
+    window.location.href = 'https://yurisokolovicz.github.io//bankist/';
+});
+
+const bankistButtonDoc = document.getElementById('bankist-button-doc');
+bankistButtonDoc.addEventListener('click', function () {
+    window.location.href = 'https://github.com/yurisokolovicz/BANKIST';
 });
